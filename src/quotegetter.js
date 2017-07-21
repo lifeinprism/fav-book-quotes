f19a4d9f525e770cf7ae4d2b8567535802711ea9
Nathan Icart <nathan.icart@gmail.com>
2017-07-21 02:59:24 -0500

    added randomizer

--- src/quotegetter.js
+++ src/quotegetter.js
@@ -3,7 +3,9 @@ window.onload = function() {
   document.getElementById("quoteButton").onclick = function() {quotes()};
 
   let quotes = function quoteGetter() {
-  	document.getElementById("quoteDiv").innerHTML = 'Here are some words';
-  }
+  	let randomBook = Math.floor(Math.random() * quotesAndAuthor.length);
+  	document.getElementById("quoteDiv").innerHTML = quotesAndAuthor[randomBook].quote;
 
+  	console.log(randomBook)
+  }
 };
