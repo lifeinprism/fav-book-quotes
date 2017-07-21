0c2a42f985292820a87c61442caed069fb9d8daa
Nathan Icart <nathan.icart@gmail.com>
2017-07-21 07:14:01 -0500

    added array elements

--- src/books.js
+++ src/books.js
@@ -3,5 +3,10 @@ var quotesAndAuthor = [
   {quote: 'Never trust anything that can think for itself if you can\'t see where it keeps its brain.', author: 'J.K. Rowling', title: 'Harry Potter and the Chamber of Secrets'},
   {quote: 'Beware of the man who works hard to learn something, learns it, and finds himself no wiser than before.', author: 'Kurt Vonnegut', title: 'Cat\'s Cradle'},
   {quote: 'I believe a strong woman may be stronger than a man, particularly if she happens to have love in her heart. I guess a loving woman is indestructible.', author: 'John Steinbeck', title: 'East of Eden'},
-  {quote: 'Life was such a wheel that no man could stand upon it for long. And it always, at the end, came round to the same place again.', author: 'Stephen King', title: 'The Stand'}
+  {quote: 'Life was such a wheel that no man could stand upon it for long. And it always, at the end, came round to the same place again.', author: 'Stephen King', title: 'The Stand'},
+  {quote: '"And did I pass?" The face of the old woman on my right was unreadable in the gathering dusk. On my left the younger woman said, "You don\'t pass or fail at being a person, dear."', author: 'Neil Gaiman', title: 'The Ocean at the End of the Lane'},
+  {quote: 'He had noticed that events were cowards: they didn\'t occur singly, but instead they would run in packs and leap out at him all at once.', author: 'Neil Gaiman', title: 'Neverwhere'},
+  {quote: 'My favorite part of the party is when the party is over. When I don\'t feel obligated to have a good time, and I can just sit and chill with whoever\'s left to chill with, you know?', author: 'Drew Magary', title: 'The Hike'},
+  {quote: 'There is not love of life without despair about life.', author: 'Albert Camus', title: 'The Stranger'},
+  {quote: 'The story was gradually taking shape. Pilon liked it this way. It ruined a story to have it all come out quickly. The good story lay in half-told things which must be filled in out of the hearer\'s own experience.', author: 'John Steinbeck', title: 'Tortilla Flat'}
 ];
