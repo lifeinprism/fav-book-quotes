ef5ed26f36e31c7bf7516cf06f024bb6637caddc
Nathan Icart <nathan.icart@gmail.com>
2017-07-21 03:19:19 -0500

    added map and formatting

--- src/quotegetter.js
+++ src/quotegetter.js
@@ -1,11 +1,21 @@
 window.onload = function() {
 
   document.getElementById("quoteButton").onclick = function() {quotes()};
-
-  let quotes = function quoteGetter() {
-  	let randomBook = Math.floor(Math.random() * quotesAndAuthor.length);
-  	document.getElementById("quoteDiv").innerHTML = quotesAndAuthor[randomBook].quote;
-
-  	console.log(randomBook)
+ 
+  let bookAuthorTitle = function (object, index) {
+    return object.quote + '<br>' + 'from:' + object.title + '<br>' + 'by: ' + object.author;
   }
+
+  let quotes = function quoteGetter() { 
+  	let randBook = Math.floor(Math.random() * quotesAndAuthor.length);
+  	console.log(randBook);
+  	document.getElementById("quoteDiv").innerHTML = quotesAndAuthor.map(bookAuthorTitle)[randBook];
+ 
+  }
+  
 };
+
+
+
+//  used to print the book quote to the screen
+// 	document.getElementById("quoteDiv").innerHTML = quotesAndAuthor[randomBook].quote;
